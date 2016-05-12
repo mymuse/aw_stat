a5a933bfc2497155ddbd535aca39cde78a351803
Alona_Chuchalina <Alona_Chuchalina@epam.com>
2016-05-12 18:04:39 +0300

    mongo db integration

---
+++ hb_server/server-client.js
@@ -0,0 +1,52 @@
+var http = require('http');
+var Static = require('node-static');
+var WebSocketServer = new require('ws');
+
+var clients = {};
+
+var webSocketServer = new WebSocketServer.Server({port: 8085});
+
+webSocketServer.on('connection', function(ws) {
+    var isRegistered = false;
+    var id = Math.random();
+    clients[id] = ws;
+    console.log("new connection " + id);
+    
+
+  ws.on('message', function(message) {
+    var event = JSON.parse(message);
+    if (event.type === 'authorize') {
+        console.log('authorize: ' + event.id);
+        checkId(event.id, function (success) {
+            isRegistered = success;
+            var returning = {type:'authorize', success: success};
+            ws.send (JSON.stringify(returning));
+        });
+
+    }
+    if(!isRegistered){
+        console.log('AUTORIZATION is needed!');
+        var returning = {type:'authorize', success: success};
+        ws.send (JSON.stringify(returning));
+    } else {
+        if(event.type === 'message'){
+             console.log('MESSAGE--> ' + event.text); 
+        }
+    }
+  });
+
+  ws.on('close', function() {
+    console.log('connection closed ' + id);
+    delete clients[id];
+  });
+
+});
+
+function checkId (id, callback) {
+    callback (id === "123");
+}
+   
+       
+console.log("Server runs on port 8085");
+
+
