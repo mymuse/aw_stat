7b3302b827ef4f2127b5fc9b91a8659b4d3122e5
Alona_Chuchalina <Alona_Chuchalina@epam.com>
2016-05-12 23:19:25 +0300

    MongoDB integration successfully implemented

--- hb_server/server.js
+++ hb_server/server.js
@@ -1,36 +1,43 @@
 var http = require('http');
 var Static = require('node-static');
-var WebSocketServer = new require('ws');
+var WebSocketServer = new require('ws'),
+    webSocketServer = new WebSocketServer.Server({port: 8085});
+
+
+var MongoClient = require('mongodb').MongoClient;   
+var hbstatBD;
+MongoClient.connect('mongodb://127.0.0.1:27017/hb_stat', function (err, db) {
+	if (err) {throw err}
+	console.log("connection to bd is successfull");
+	hbstatBD = db.collection('hbstat');
+});
 
 var clients = {};
 
-var webSocketServer = new WebSocketServer.Server({port: 8085});
-
 webSocketServer.on('connection', function(ws) {
-    var isRegistered = false;
+    var currentId;
     var id = Math.random();
     clients[id] = ws;
     console.log("new connection " + id);
-    
 
   ws.on('message', function(message) {
     var event = JSON.parse(message);
     if (event.type === 'authorize') {
         console.log('authorize: ' + event.id);
         checkId(event.id, function (success) {
-            isRegistered = success;
+            currentId = event.id; 
             var returning = {type:'authorize', success: success};
             ws.send (JSON.stringify(returning));
         });
-
     }
-    if(!isRegistered){
-        console.log('AUTORIZATION is needed!');
-        var returning = {type:'authorize', success: success};
-        ws.send (JSON.stringify(returning));
-    } else {
+     else {
+        if(!currentId){
+            console.log('AUTORIZATION is needed!');
+            var returning = {type:'authorize', success: false};
+            ws.send (JSON.stringify(returning));
+        }
         if(event.type === 'message'){
-             console.log('MESSAGE--> ' + event.text); 
+            pushEvent(currentId, Math.random(), event.text);
         }
     }
   });
@@ -42,10 +49,16 @@ webSocketServer.on('connection', function(ws) {
 
 });
 
-function checkId (id, callback) {
-    callback (id === "123");
+function checkId(id, callback) {
+    hbstatBD.find({id: id}).toArray(function (error, list) {
+        console.log(list);
+		callback (list.length !== 0);
+	});
 }
-   
+
+function pushEvent (id, eventId, message, callback) {
+    hbstatBD.update({id:id}, {$push:{events:{eventId: eventId, message: message}}});
+}
        
 console.log("Server runs on port 8085");
 
