f3847d0d6b6a8a1350d5e6ab4d230df94d25ccbc
Alona_Chuchalina <Alona_Chuchalina@epam.com>
2016-05-23 18:03:32 +0300

    Code refactor

--- pages/login/login.js
+++ pages/login/login.js
@@ -1,3 +1,14 @@
-hbStatApp.controller('loginController', function($scope) {
+hbStatApp.controller('loginController', function($scope, $location) {
+    $scope.go = function ( path ) {
+        $location.path( path );
+    };
+    
+    $scope.submit = function () {
+       var user = $scope.user;
+    };
+    
+    function _validation(user) {
+        
+    };
     
 });
